f1015badd68676729d98a52d45a1e884916f58fe
DanielCs1988 <daniel.cs20@gmail.com>
2018-02-26 19:41:46 +0100

    Finished python part and html layout.

---
+++ server.py
@@ -0,0 +1,67 @@
+from flask import Flask, render_template, session, request, redirect, url_for
+from flask_socketio import SocketIO, emit, disconnect
+
+app = Flask(__name__)
+
+# Setting the secret key is a necessity for SocketIO. You should fetch this from an envvar normally.
+app.secret_key = 'super_secret_key'
+app.debug = True
+
+# Creating an instance of the SocketIO object, we pass the current app as a parameter.
+# This will act as our API to interact with the socket connections.
+socket = SocketIO(app)
+
+# Just to spare you the effort of setting up a database for this small demo.
+# This also means that the chat history will be purged when the server shuts down.
+chat_history = []
+
+
+@app.route('/')
+def login_page():
+    # We can still use normal HTTP flask routes just like before.
+    # We should also serve individual HTML files this way.
+    return render_template('login.html')
+
+
+@app.route('/login', methods=['POST'])
+def login_user():
+    # Please note that SocketIO alters the way session behaves, so we should assert it's state
+    # (handle the login attempt) before opening up the socket connection.
+    nickname = request.form['nickname']
+    if len(nickname) >= 5:
+        session['user'] = nickname
+        return render_template('index.html')
+
+    return redirect(url_for('login_page'))
+
+
+@socket.on('connect')
+def init_socket_connection():
+    # Connect is the default event that is fired when a client opens a socket connection.
+    # The script does not allow unauthorized connection, but the console can be exploited to bypass that,
+    # so we need this extra layer of security. Disconnect cuts the socket connection.
+    if not session.get('user'):
+        disconnect()
+
+
+@socket.on('chat')
+def handle_incoming_chat_message(chat_msg):
+    # First we save the message to the database.
+    chat_history.append(chat_msg)
+    # Then we broadcast the message to all the connected clients, except for the sender.
+    emit('chat', chat_msg, broadcast=True, include_self=False)
+    # It's nice to give a feedback to the sender about the successful data transfer.
+    # This message is overly verbose for demonstration purposes. See the script side for more info.
+    return 'Message received and saved.'
+
+
+@socket.on('text-edited')
+def update_cards(text):
+    # We are broadcasting the current state of the paragraph after each and every keystroke.
+    # Providing a return value - aka feedback to the sender - is not necessary as you can see.
+    emit('text-edited', text, broadcast=True, include_self=False)
+
+
+if __name__ == '__main__':
+    # Please note that we have to run the socket, not the app.
+    socket.run(app)
